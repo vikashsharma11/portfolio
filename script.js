b179d8ccde7a8e438f16e4c97a65ef7f82a5346a
Xiber <vikash.shm.vs07@gmail.com>
2023-08-27 02:13:15 +0530

    javascript changed

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-var typed = new Typed(".multiple-text",{
+/*var typed = new Typed(".multiple-text",{
     strings:["Frontend Developer","Dot Net Developer","MERN Stack Developer","YouTuber"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 
-})  
+});*/
+
+var typed = new Typed('.multiple-text', {
+    strings: ["First sentence.", "Second sentence."],
+    typeSpeed: 30
+  });
